35d92cf7b5741339174d5954fec92aebc0bc07e4
yann <yann.cla@gmail.com>
2022-01-04 18:55:52 +0100

    Update settings.py

--- django-cookbook/recipe/recipe/settings.py
+++ django-cookbook/recipe/recipe/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cookbook'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'recipe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cookbook',
+        'USER': 'recipe',
+        'PASSWORD': 'admin',
+        'HOST': '192.168.1.10',
+        'PORT': '3306'
     }
 }
 
