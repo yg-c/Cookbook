807fad2d6203e1b851b284f1fe647b0d681ed14d
yann <yann.cla@gmail.com>
2022-01-05 17:17:10 +0100

    Update forms.py

--- django-cookbook/recipe/cookbook/forms.py
+++ django-cookbook/recipe/cookbook/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import *
 
 
-class UnitForm(forms.Form):
+class UpdateUnitForm(forms.Form):
     id = forms.IntegerField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    name = forms.CharField(label='Nom', max_length=100)
+
+
+class AddUnitForm(forms.Form):
     name = forms.CharField(label='Nom', max_length=100)
