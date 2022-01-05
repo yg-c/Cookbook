687a3f3a7a22d4412a86123a79e00fbebae8f920
yann <yann.cla@gmail.com>
2022-01-05 17:54:14 +0100

    Update views.py

--- django-cookbook/recipe/cookbook/views.py
+++ django-cookbook/recipe/cookbook/views.py
@@ -61,7 +61,6 @@ def addunit(request):
 
 
 def deleteunit(request, pk):
-    print(pk)
     MeasurementUnit.objects.using('default').filter(id=pk).delete()
     return HttpResponseRedirect('/cookbook/units')
 
