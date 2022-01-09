656f0c7b662341b92c51b919d4ba52518e3a7c20
yann <yann.cla@gmail.com>
2022-01-09 09:37:08 +0100

    UpdateIngredientForm

--- django-cookbook/recipe/cookbook/forms.py
+++ django-cookbook/recipe/cookbook/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import *
 
 
+# region units
 class UpdateUnitForm(forms.Form):
     id = forms.IntegerField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
     name = forms.CharField(label='Nom', max_length=100)
@@ -9,3 +10,13 @@ class UpdateUnitForm(forms.Form):
 
 class AddUnitForm(forms.Form):
     name = forms.CharField(label='Nom', max_length=100)
+
+# endregion units
+
+
+# region ingredients
+class UpdateIngredientForm(forms.Form):
+    id = forms.IntegerField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    name = forms.CharField(label='Nom', max_length=100)
+
+# endregion ingredients
