6f0108e15f84ab637fa6e2315e4a60848aaedf99
yann <yann.cla@gmail.com>
2022-02-01 11:17:03 +0100

    Update forms.py

--- django-cookbook/recipe/cookbook/forms.py
+++ django-cookbook/recipe/cookbook/forms.py
@@ -7,23 +7,8 @@ from .models import *
 class AddRecipeForm(forms.Form):
     name = forms.CharField(label='Nom', max_length=100)
 
-
-class SortRecipesByCourse(forms.Form):
-    # Création de la liste déroulante des plats (une liste ne peut-être que sous forme de tuples)
-    courses = Course.objects.all()
-    course_name_list = []
-    course_id_list = []
-
-    for c in courses:
-        course_id_list.append(c.id)
-        course_name_list.append(c.name)
-
-    course_tuple = list(zip(course_id_list, course_name_list))
-
-    # Renvoi de l'id
-    course_choice = forms.IntegerField(label='Choix du plat', widget=forms.Select(choices=course_tuple))
-
 # endregion recipes
+
 
 # region units
 class UpdateUnitForm(forms.Form):
